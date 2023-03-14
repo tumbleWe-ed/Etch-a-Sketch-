45c6cbd384529ddbd57e7a3d34fd9861bdd2ae81
tumbleweed <tumbleWeed_06@proton.me>
2023-03-14 22:11:15 +0530

    event for drawing

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ let divContainer = document.querySelector(".grid-container");
 
 let gridMember
 
+let isMouseDown
+let isMouseOver
+
+
 for (let i = 0; i < (16*16) ; i++) {
-    console.log(i);
+
     gridMember = document.createElement("div");
     divContainer.append(gridMember)
-    gridMember.style.cssText = "padding: 10px; background-color: white; border: black solid; border-width: 1px"
+    gridMember.style.cssText = "padding: 10px;background-color: white; border: black solid; border-width: 1px"
+    gridMember.setAttribute("draggable", false)
+
+    gridMember.addEventListener('mouseup', () => {isMouseDown = false})
+    gridMember.addEventListener('mousedown', () => { isMouseDown = true})
+
+    gridMember.addEventListener('mouseover', (event) => {isMouseOver=true 
+        if (isMouseDown && isMouseOver) {
+        event.target.style.backgroundColor = 'black'
+        }
+      })
+
+
+
+    
 }
 
